5837603678a8e8aa88640189424d600ca3791578
BakiCanOzen <bakican1980@gmail.com>
2025-11-24 20:40:43 +0300

    Proje dosyası ekle.

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApi.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("ok");
+}
